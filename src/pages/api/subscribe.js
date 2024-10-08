f81587269a76ecc065aae21000708f2dbab25fd3
Đình Thắng <thang.hoang1307@gmail.com>
2024-10-08 11:54:16 +0700

    Update

--- src/pages/api/subscribe.js
+++ src/pages/api/subscribe.js
@@ -6,7 +6,7 @@ export default function handler(req, res) {
       'method': 'POST',
       'url': `https://graph.facebook.com/${page.id}/subscribed_apps?subscribed_fields=feed&access_token=${page.access_token}`
     }).then(response => {
-      response.status(200);
+      res.status(200);
     })
       .catch(error => {
         console.error('Error fetching data:', err);

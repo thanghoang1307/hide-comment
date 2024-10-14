390c54f5715cb321f398429536cbdf583426c2e5
Đình Thắng <thang.hoang1307@gmail.com>
2024-10-14 11:38:56 +0700

    Update

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default function handler(req, res) {
-    const url = `https://graph.facebook.com/v21.0/563169416286258_887469166656567?is_hidden=true&access_token=EAARU93ZChWsABO0Enaq6ZBNiOfN7CGj3UXYkFtnkYtC9SVqFZBI6JyR37MLH8wo8tCD0QeTl6wwFXqoPyHw6uPfiuyS78AA90TluMA6pH0UApVrm4SvcKAQObkGvVeni0ZCgEQJhObCW7fNAJEfWtD3iO2HZAkcvEZAl8udhHn13UHi1ZBTHHzJb3ljeEu6V8oZD`;
+    const url = `https://graph.facebook.com/v21.0/564477716155428_991464686079777?is_hidden=true&access_token=EAARU93ZChWsABO0Enaq6ZBNiOfN7CGj3UXYkFtnkYtC9SVqFZBI6JyR37MLH8wo8tCD0QeTl6wwFXqoPyHw6uPfiuyS78AA90TluMA6pH0UApVrm4SvcKAQObkGvVeni0ZCgEQJhObCW7fNAJEfWtD3iO2HZAkcvEZAl8udhHn13UHi1ZBTHHzJb3ljeEu6V8oZD`;
         
         axios.post(url, null, {timeout: 10000})
           .then(response => {

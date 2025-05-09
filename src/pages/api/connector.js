efecb147a69644da49223d2489462b768401ee0c
admin <thang.hoang1307@gmail.com>
2025-05-09 09:27:58 +0700

    Stop hiding comment on Mas Corporate

--- src/pages/api/connector.js
+++ src/pages/api/connector.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export default function handler(req, res) {
-    const {comment_id} = req.query;
     const url = `https://graph.facebook.com/v21.0/${comment_id}?is_hidden=true&access_token=EAARU93ZChWsABO0Enaq6ZBNiOfN7CGj3UXYkFtnkYtC9SVqFZBI6JyR37MLH8wo8tCD0QeTl6wwFXqoPyHw6uPfiuyS78AA90TluMA6pH0UApVrm4SvcKAQObkGvVeni0ZCgEQJhObCW7fNAJEfWtD3iO2HZAkcvEZAl8udhHn13UHi1ZBTHHzJb3ljeEu6V8oZD`;
         
         axios.post(url, null, {is_hidden: true, timeout: 10000})
